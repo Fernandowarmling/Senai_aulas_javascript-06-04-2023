4104ab15403be80a939d44e7444b8d42fee45152
Fernando Warmling <100279125+Fernandowarmling@users.noreply.github.com>
2023-04-08 09:43:11 -0300

    Add files via upload

---
+++ desafiosjavascript/scripts/jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("header").load("/pages/general/header.html")
+    $("footer").load("/pages/general/footer.html")
+    $("nav").load("/pages/general/nav.html")
+    
+})
