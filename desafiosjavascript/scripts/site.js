b63c2345c60cbe7ae92a7746de0d8d447dd7c7c4
fernandowarmling <warmlingfernando@gmail.com>
2023-04-11 21:49:14 -0300

    change: mudanças de input de dados

---
+++ desafiosjavascript/scripts/site.js
@@ -0,0 +1,16 @@
+function escolha() {
+
+    switch (escolha) {
+        case "Suco":
+            console.log("Escolha correta" + escolha)
+            break;
+        case "Refrigerante":
+            console.log("Escolha correta " + escolha)
+            break;
+        case "Sorvete":
+            console.log("Escolha Correta" + escolha)
+            break;
+        default:
+            console.log("Produto Egotado por favor selecione outro")
+    }
+}
